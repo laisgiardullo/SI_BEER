7ec6218839e69b02c910a0a01f837c6c666aa5e7
tchantchong <tchantchong@gmail.com>
2016-06-08 11:19:59 -0300

    Classes

--- beersite/webapp/models.py
+++ beersite/webapp/models.py
@@ -11,10 +11,12 @@ class Tipo(models.Model):
 
 class Cerveja(models.Model):
 	nome = models.CharField(max_length = 40, blank = True, null = True)
+	marca = models.CharField(max_length = 40, blank = True, null = True)
 	tipo = models.ManyToManyField(Tipo) 
 	fornecedor = models.CharField(max_length = 120)
 	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)
-	last_visit = models.DateTimeField(auto_now_add = False, auto_now = True)
+	preco = models.DecimalField(max_digits=5, decimal_places=2)
+	quantidadeNoPacote = AutoField(max_digits = 2)
 	descricao = models.TextField()
 
 	def __unicode__(self):
@@ -31,8 +33,8 @@ class Usuario(models.Model):
 		
 class Combinacao(models.Model):
 	nome = models.CharField(max_length = 40, blank = True, null = True)
-	status = models.CharField(max_length = 40)	
-	
+	status = models.CharField(max_length = 40)
+
 	def __unicode__(self):
 		return self.user.username
 
@@ -41,9 +43,42 @@ class Pacote(models.Model):
 	valor = models.DecimalField(max_digits=7, decimal_places=2)
 	frequencia = models.CharField(max_length = 40, blank = True, null = True)
 	descricao = models.CharField(max_length = 1000, blank = True, null = True)
+
+	def __unicode__(self):
+		return self.user.username
+
+class Assinatura(models.Model):
+	dataInicio = models.DateField(auto_now = False, auto_now_add = False)
+	status = models.CharField(max_length = 40)
+
+	def __unicode__(self):
+		return self.user.username
+
+class Conteudo(models.Model):
+	quantidade = models.AutoField(max_digits=6)
+
+	def __unicode__(self):
+		return self.user.username
+
+class Itens(models.Model):
+	quantidade = models.AutoField(max_digits=6)
+
+	def __unicode__(self):
+		return self.user.username
+
+class Pedido(models.Model):
+	orcamento = models.DecimalField(max_digits=8, decimal_places=2)
+	data = models.DateField(auto_now = False, auto_now_add = False)
+	status = models.CharField(max_length = 40)
+	frete = models.DecimalField(max_digits=5, decimal_places=2)
 	
 	def __unicode__(self):
 		return self.user.username
-		
-class Assinatura(models.Model):
-	dataInicio = models
+
+class Fornecedor(models.Model):
+	CNPJ = models.AutoField(max_digits = 14)
+	endereco = models.TextField()
+	status = models.CharField(max_length = 40)
+	
+	def __unicode__(self):
+		return self.user.username
