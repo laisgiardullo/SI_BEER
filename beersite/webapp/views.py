f3d596d7329933de64932adcd8e6e742c7bdcc56
guimatsumoto <guilhermekmatsumoto@gmail.com>
2016-06-15 11:42:11 -0300

    alteracao do preco das cervejas implementado

--- beersite/webapp/views.py
+++ beersite/webapp/views.py
@@ -10,6 +10,7 @@ from webapp.models import Usuario, Fornecedor, Cerveja, Tipo, Administrador, Pac
 from webapp.forms import UserForm, UserProfileForm, FornecedorForm, BeerForm, PacoteForm, CombinacaoForm
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from django.views.generic.edit import UpdateView
 
 def home(request):
 	return render(request, 'webapp/home.html')
@@ -301,6 +302,40 @@ def delete_account(request):
 	user.delete()
 	return render_to_response('webapp/conta_cancelada.html', {'full_name': username})
 
+def show_beer(request):
+	context = RequestContext(request)
+	cerveja = Cerveja.objects.all()
+	return render_to_response('webapp/show_beer.html', {'cerveja': cerveja}, context)
 
+@csrf_protect
+def del_beer(request):
+	context = RequestContext(request)
+	if request.method == 'POST':
+		delete = request.POST.getlist('checks')
+		cervejas = Cerveja.objects.filter(id__in=delete)
+		for i in cervejas:
+			i.delete()
+		aux = Cerveja.objects.all()
+		return render_to_response('webapp/show_beer.html', {'cerveja': aux}, context)
+	return render_to_response('webapp/home.html')
 
+@csrf_protect
+def change_beer(request):
+	context = RequestContext(request)
+	cervejas = Cerveja.objects.all()
+	return render_to_response('webapp/change_beer.html', {'cerveja': cervejas}, context)
+
+@csrf_protect
+def altera_cerveja(request):
+	context = RequestContext(request)
+	if request.method == 'POST':
+		change = request.POST.getlist('checks')
+		cervejas = Cerveja.objects.filter(id__in=change)
+		for i in cervejas:
+			label_preco = 'preco_' + str(i.id)
+			i.preco = request.POST.get(label_preco)
+			i.save()
+		aux = Cerveja.objects.all()
+		return render_to_response('webapp/change_beer.html', {'cerveja': aux}, context)
+	return render_to_response('webapp/home.html')
 
